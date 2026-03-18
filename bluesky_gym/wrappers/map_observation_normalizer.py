b6395c9050e986594452f9d35d91b31b1fe92fc9
Twan <118354671+TwanvanDam@users.noreply.github.com>
2026-03-18 16:42:46 +0100

    Refactor log normalization calculation in map observation normalizer

--- bluesky_gym/wrappers/map_observation_normalizer.py
+++ bluesky_gym/wrappers/map_observation_normalizer.py
@@ -30,7 +30,7 @@ class MapObservationNormalizer(gym.ObservationWrapper):
                 value = observation_copy.pop(key)
                 match self.mode:
                     case "log":
-                        observation_copy[key] = np.clip(np.log1p(value / self.env.map_source_max), 0, 1)
+                        observation_copy[key] = np.clip(np.log1p(value) / np.log1p(self.env.map_source_max), 0, 1)
                     case "min-max":
                         observation_copy[key] = np.clip(value / self.env.map_source_max, 0, 1)
                     case _:
