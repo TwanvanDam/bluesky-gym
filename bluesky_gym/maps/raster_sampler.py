88d402dbc5a2df31c06db742bae2decdb0ae7646
Twan <118354671+TwanvanDam@users.noreply.github.com>
2026-03-19 19:52:54 +0100

    Clamp raster values to non-negative range in map extraction

--- bluesky_gym/maps/raster_sampler.py
+++ bluesky_gym/maps/raster_sampler.py
@@ -87,4 +87,5 @@ class RasterSampler:
         dst_transform = self.get_dst_transform_from_bounds(x_min, y_min, x_max, y_max, width, height)
         map_extract = self._extract_view_from_map(dst_transform, (width, height))
         map_extract = np.where(map_extract < -100, np.nan, map_extract)
+        map_extract = np.clip(map_extract, 0, None)
         return map_extract
